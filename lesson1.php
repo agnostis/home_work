c0857603c64c89e978f393f90ce37e04a383d7e3
Andrii_Zhuvak <emandem757@gmail.com>
2021-01-07 22:08:07 +0200

    change files

--- lesson1.php
+++ lesson1.php
@@ -7,26 +7,33 @@
 </head>
 <body>
 <?php
-makeTable(5,10,1, "table");
-makeTable(5,10,6,"table2");
+echo '<div>';
+makeTable(5, 10, 1);
+makeTable(5, 10, 6);
+echo '</div>';
 
-
-function makeTable($row1,$column1,$start, $class){
-    echo '<table class="'.$class.'">';
+function makeTable($row1, $column1, $start)
+{
+    echo '<table>';
     echo '<tr>';
-    if ($start > $row1){
-        for ($row = 10; $row >= 6; $row--){
+    if ($start > $row1)
+    {
+        for ($row = 6;$row <= 10;$row++)
+        {
             echo '<td>';
-            for ($column = 1; $column <= $column1; $column++) {
+            for ($column = 1;$column <= $column1;$column++)
+            {
                 echo $row . ' x ' . $column . ' = ' . $column * $row;
                 echo '</br>';
             }
             echo '</td>';
         }
     }
-    for ($row = $start; $row <= $row1; $row++) {
-        echo '<td>'; // создаем td
-        for ($column = 1; $column <= $column1; $column++) {
+    for ($row = $start;$row <= $row1;$row++)
+    {
+        echo '<td>';
+        for ($column = 1;$column <= $column1;$column++)
+        {
             echo $row . ' x ' . $column . ' = ' . $column * $row;
             echo '</br>';
         }
--- lesson2.php
+++ lesson2.php
@@ -7,41 +7,50 @@
 </head>
 <body>
 <?php
-makeTable(5,10,1, "table");
-makeTable(5,10,6,"table2");
-
-function makeTable($row1,$column1,$start,$class){
-    echo '<table class="'.$class.'">';
+echo '<div>';
+makeTable(5, 10, 1);
+makeTable(5, 10, 6);
+echo '</div>';
+function makeTable($row1, $column1, $start)
+{
+    echo '<table>';
     echo '<tr>';
-    if ($start > $row1){
-        for ($row = 6; $row <= 10; $row++){
+    if ($start > $row1)
+    {
+        for ($row = 6;$row <= 10;$row++)
+        {
             echo '<td>';
-            for ($column = 1; $column <= 10; $column++) {
+            for ($column = 1;$column <= 10;$column++)
+            {
                 $temp = $row * $column;
-                if (strlen($temp) >= 2){
+                if (strlen($temp) >= 2)
+                {
                     $first = floor($temp / 10);
                     $second = $temp % 10;
-                    echo changeColor($row) .' x '. changeColor($column) .' = '.changeColor($first) . changeColor($second);
+                    echo changeColor($row) . ' x ' . changeColor($column) . ' = ' . changeColor($first) . changeColor($second);
                     echo '</br>';
                     $first = 0;
                     $second = 0;
                     continue;
                 }
                 $second = $temp % 10;
-                echo changeColor($row) .' x '. changeColor($column) .' = '. changeColor($second);
+                echo changeColor($row) . ' x ' . changeColor($column) . ' = ' . changeColor($second);
                 echo '</br>';
             }
             echo '</td>';
         }
     }
-    for ($row = $start; $row <= $row1; $row++) {
-        echo '<td>'; // создаем td
-        for ($column = 1; $column <= $column1; $column++) {
+    for ($row = $start;$row <= $row1;$row++)
+    {
+        echo '<td>';
+        for ($column = 1;$column <= $column1;$column++)
+        {
             $temp = $row * $column;
-            if (strlen($temp) >= 2){
+            if (strlen($temp) >= 2)
+            {
                 $first = floor($temp / 10);
                 $second = $temp % 10;
-                echo changeColor($row) .' x '. changeColor($column) .' = '.changeColor($first) . changeColor($second);
+                echo changeColor($row) . ' x ' . changeColor($column) . ' = ' . changeColor($first) . changeColor($second);
                 echo '</br>';
                 $first = 0;
                 $second = 0;
@@ -49,7 +58,7 @@ function makeTable($row1,$column1,$start,$class){
             }
             $first = floor($temp / 10);
             $second = $temp % 10;
-            echo changeColor($row) .' x '. changeColor($column) .' = '. changeColor($second);
+            echo changeColor($row) . ' x ' . changeColor($column) . ' = ' . changeColor($second);
             echo '</br>';
         }
         echo '</td>';
@@ -58,25 +67,27 @@ function makeTable($row1,$column1,$start,$class){
     echo '</tr>';
     echo '</table>';
 }
-function changeColor($num){
+function changeColor($num)
+{
     $red = '<span style="color:red">';
     $green = '<span style="color:green">';
     $yellow = '<span style="color:yellow">';
-    $blue= '<span style="color:blue">';
-    $num = $num == 10 ? $red . (floor($num / 10) .'</span>' . 0) : $num;
+    $blue = '<span style="color:blue">';
+    $num = $num == 10 ? $red . (floor($num / 10) . '</span>' . 0) : $num;
 
-    switch ($num){
+    switch ($num)
+    {
         case 1:
-            $num = $red . $num. '</span>';
+            $num = $red . $num . '</span>';
             break;
         case 2:
-            $num = $green . $num. '</span>';
+            $num = $green . $num . '</span>';
             break;
         case 3:
-            $num = $yellow . $num. '</span>';
+            $num = $yellow . $num . '</span>';
             break;
         case 4:
-            $num = $blue . $num. '</span>';
+            $num = $blue . $num . '</span>';
             break;
     }
     return $num;
